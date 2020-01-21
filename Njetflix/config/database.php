db818fe40438eefeb8783842350b4815d4bfa32a
Hundewurst <58326801+Hundewurst@users.noreply.github.com>
2020-01-21 12:26:33 +0100

    Add files via upload

---
+++ Njetflix/config/database.php
@@ -0,0 +1,22 @@
+<?php
+
+$dbname = 'njetflix_n';
+
+$dns = 'mysql:dbname='.$dbname.';host=localhost';
+$user = 'root';
+$pw   = '';
+$options =[
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+];
+
+$db = null;
+
+try
+{
+    $db = new PDO($dns, $user, $pw, $options);
+}
+catch (PDOException $e)
+{
+    die('Database connection failed: '.$e->getMessage());
+}
---
+++ Njetflix/index.php
@@ -0,0 +1,64 @@
+<?php
+
+require_once 'config/database.php';
+
+if(isset($_POST['create']))
+{
+	$sqlStr = 'INSERT INTO account (firstname, lastname, email, passwordHash) VALUES (:firstname, :lastname, :email, :passwordHash)';
+  	$stmt = $database->prepare($sqlStr);
+
+  	$stmt->execute([
+		'firstname' => $_POST['firstname'],
+		'lastname' => $_POST['lastname'],
+		'email' => $_POST['email'],
+		'passwordHash' => password_hash($_POST['password'], PASSWORD_DEFAULT)
+  	]);
+}
+
+
+$accounts = null;
+$accountsErrorMessage = false;
+try
+{
+    // Execute the SQL Statement and fetch data
+    $accounts = $database->query('SELECT * FROM account WHERE 1')->fetchAll();
+}
+catch (\PDOException $e)
+{
+    $accountsErrorMessage = true;
+}
+
+
+?>
+<html>
+	<head>
+		<title>Meine PDO Welt</title>
+	</head>
+	<body>
+		<header></header>
+		<main>
+			<?php if($accountsErrorMessage === true) : ?>
+			<div class="error">
+				Ja, mhhh irgendwie bekomm ich die Daten aus der Datenbank nicht!! Schau mal bitte nach.
+			</div>
+			<?php endif; ?>
+			<form method="post">
+				<lable>Firstname</lable><br />
+				<input type="text" name="firstname" value="<?=isset($_POST['firstname'])?htmlspecialchars($_POST['firstname']):''?>"/><br />
+				<lable>Lastname</lable><br />
+				<input type="text" name="lastname" value="<?=isset($_POST['lastname'])?htmlspecialchars($_POST['lastname']):''?>"/><br />
+				<lable>E-Mail</lable><br />
+				<input type="text" name="email" value="<?=isset($_POST['email'])?htmlspecialchars($_POST['email']):''?>"/><br />
+				<lable>Password</lable><br />
+				<input type="password" name="password"/><br /><br />
+				<input type="submit" name="create" value="Anlegen"/>
+			</form>				
+			<ul>
+			<?php foreach($accounts as $account) : ?>
+				<li><?=$account['firstname'].' '.$account['lastname']?></li>
+			<?php endforeach; ?>
+			</ul>
+		</main>
+		<footer></footer>
+	</body>
+</html>
